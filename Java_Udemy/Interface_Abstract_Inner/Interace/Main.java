919995e654cd8787e21825c4299ecbbef2e728f2
Sonukumar.Gupta <Sonukumar.gupta@XEROX.com>
2021-07-04 18:43:11 +0530

    java

---
+++ Java_Udemy/Interface_Abstract_Inner/Interace/Main.java
@@ -0,0 +1,20 @@
+
+package Interace;
+
+public class Main {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		ITelephone guptaPhone;
+//		guptaPhone=new DeskPhone(1234567);
+//		guptaPhone.powerOn();
+//		guptaPhone.callPhone(2314566);
+//		guptaPhone.answer();
+//		
+//		guptaPhone=new MobilePhone(234342);
+//		guptaPhone.callPhone(2314566);
+//		//guptaPhone.powerOn();
+//		guptaPhone.answer();
+	//}
+
+}
--- Java_Udemy/Interface_Abstract_Inner/Interace/MobilePhone.java
+++ Java_Udemy/Interface_Abstract_Inner/Interace/MobilePhone.java
@@ -1,29 +1,35 @@
 package Interace;
 
-public class DeskPhone implements ITelephone{
+public class MobilePhone implements ITelephone {
+	
 	private int myNumber;
 	private boolean isRinging;
+	private boolean isOn=false;
 	
-	
-	public DeskPhone(int myNumbe) {
+	public  MobilePhone(int myNumber) {
 		this.myNumber = myNumber;
 	}
 	
 	
 	@Override
 	public void powerOn() {
-		System.out.println("No action taken, desk phone does not have a power button");
+		isOn=true;
+		System.out.println("Mobile phone powered up");
 		
 	}
 	@Override
 	public void dial(int phoneNumber) {
-		System.out.println("Now ringing "+phoneNumber+" on deskphone");
-		
+		if(isOn) {
+		System.out.println("Now ringing "+phoneNumber+" on mobile phone");
+		}
+		else {
+			System.out.println("Phone is switched off");
+		}
 	}
 	@Override
 	public void answer() {
 		if (isRinging) {
-			System.out.println("Answering the desk phone");
+			System.out.println("Answering the mobile phone");
 			isRinging=false;
 		}
 	}
@@ -31,9 +37,9 @@ public class DeskPhone implements ITelephone{
 	@Override
 	public boolean callPhone(int phoneNumber) {
 		// TODO Auto-generated method stub
-		if (phoneNumber==myNumber) {
+		if (phoneNumber==myNumber &&isOn) {
 			isRinging=true;
-			System.out.println("Ring ring");
+			System.out.println("Melody ring");
 		}
 		else {
 			isRinging=false;
