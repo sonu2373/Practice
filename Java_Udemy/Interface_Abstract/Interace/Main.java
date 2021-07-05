2dabf32717895d7a903e39a5678cf554186f17e4
Sonukumar.Gupta <Sonukumar.gupta@XEROX.com>
2021-07-05 11:29:57 +0530

    Java

--- Java_Udemy/Interface_Abstract/Interace/Main.java
+++ Java_Udemy/Interface_Abstract/Interace/Main.java
@@ -1,20 +1,19 @@
-
 package Interace;
 
 public class Main {
 
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		ITelephone guptaPhone;
+		ITelephone guptaPhone;
 //		guptaPhone=new DeskPhone(1234567);
 //		guptaPhone.powerOn();
 //		guptaPhone.callPhone(2314566);
 //		guptaPhone.answer();
 //		
-//		guptaPhone=new MobilePhone(234342);
-//		guptaPhone.callPhone(2314566);
-//		//guptaPhone.powerOn();
-//		guptaPhone.answer();
-	//}
+	guptaPhone=new MobilePhone(234342);
+	guptaPhone.callPhone(2314566);
+	guptaPhone.powerOn();
+	guptaPhone.answer();
+	}
 
 }
